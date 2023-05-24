9b64100da57d63c29df78d4976562de9789dafec
sydney-rd <sydneydavid56@gmail.com>
2023-05-24 17:08:28 -0400

    adding field functionality to create

--- client/src/screens/Create/Create.jsx
+++ client/src/screens/Create/Create.jsx
@@ -1,58 +1,80 @@
-import { useRef } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { createDog } from "../../services/dogs.js";
-import '../../screens/Create/Create.css'
-
+import "../../screens/Create/Create.css";
 
 export default function Create() {
-  const dogNameRef = useRef();
-  const dogBreedRef = useRef();
-  const dogAgeRef = useRef();
-  const dogAboutRef = useRef();
-  const dogGenderRef = useRef();
-  const dogPersonalityRef = useRef();
-  const dogImageRef = useRef();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      name: dogNameRef.current.value,
-      breed: dogBreedRef.current.value,
-      age: dogAgeRef.current.value,
-      about: dogAboutRef.current.value,
-      gender: dogGenderRef.current.value,
-      personality: [dogPersonalityRef.current.value],
-      image: dogImageRef.current.value,
-    };
+  const onSubmit = async (data) => {
     await createDog(data);
     navigate("/homepage");
   };
+
   return (
     <div className="form-create">
       <h3>Create Dog Profile</h3>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Dog's Name" ref={dogNameRef}></input>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Dog's Name"
+          {...register("name", { required: true })}
+        />
+        {errors.name && <span>Name is required.</span>}
 
-        <input type="text" placeholder="Dog's Breed" ref={dogBreedRef}></input>
-        <input type="text" placeholder="Dog's Age" ref={dogAgeRef}></input>
+        <input
+          type="text"
+          placeholder="Dog's Breed"
+          {...register("breed", { minLength: 2 })}
+        />
+        {errors.breed && errors.breed.type === "minLength" && (
+          <span>Breed should be at least 2 characters long.</span>
+        )}
+
+        <input
+          type="number"
+          placeholder="Dog's Age"
+          {...register("age", { required: true, pattern: /^\d+$/ })}
+        />
+        {errors.age && errors.age.type === "required" && (
+          <span>Age is required.</span>
+        )}
+        {errors.age && errors.age.type === "pattern" && (
+          <span>Age must be an integer.</span>
+        )}
+
         <input
           type="text"
           placeholder="Tell us about yourself"
-          ref={dogAboutRef}></input>
+          {...register("about", { minLength: 24 })}
+        />
+        {errors.about && errors.about.type === "minLength" && (
+          <span>About should be at least 24 characters long.</span>
+        )}
 
-        <input type="text" placeholder="Dog's Gender" ref={dogGenderRef}></input>
+        <input
+          type="text"
+          placeholder="Dog's Gender"
+          {...register("gender", { required: true, pattern: /^(Male|Female)$/i })}
+        />
+        {errors.gender && errors.gender.type === "required" && (
+          <span>Gender is required.</span>
+        )}
+        {errors.gender && errors.gender.type === "pattern" && (
+          <span>Gender must be Male or Female.</span>
+        )}
 
         <input
           type="text"
           placeholder="Dog's Personality"
-          ref={dogPersonalityRef}
-        ></input>
+          {...register("personality")}
+        />
 
-        <input type="text" placeholder="URL" ref={dogImageRef}></input>
+        <input type="text" placeholder="URL" {...register("image")} />
 
-        <input className="btn" type="submit" value="Submit"></input>
-      </form>{" "}
+        <input className="btn" type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
