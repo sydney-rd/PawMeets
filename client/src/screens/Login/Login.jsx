38951d338c961f5e199e0e000a74e55b9008ad25
sydney-rd <sydneydavid56@gmail.com>
2023-05-24 15:28:15 -0400

    utilizing form for signup/updating css

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { signIn } from "../../services/users.js";
 import { useNavigate } from "react-router-dom";
+import './Login.css'
 
 function Login(props) {
   const { setUser } = props;
@@ -22,111 +23,66 @@ function Login(props) {
     });
   };
 
-  const validateForm = () => {
-    const { username, password } = form;
-
-    if (username.includes("-")) {
-      setForm({
-        ...form,
-        isError: true,
-        errorMsg: "Username must not contain dashes",
-      });
-      return false;
-    }
-
-    if (username.length <= 5) {
-      setForm({
-        ...form,
-        isError: true,
-        errorMsg: "Username must be over 5 characters",
-      });
-      return false;
-    }
-
-    if (password.length < 6 || !/\d/.test(password) || !/[!@#$%^&*]/.test(password)) {
-      setForm({
-        ...form,
-        isError: true,
-        errorMsg: "Password must be at least 6 characters long and include a symbol and a number",
-      });
-      return false;
-    }
-
-    return true;
-  };
-
-  const onLogin = async (e) => {
+  const onSignIn = async (e) => {
     e.preventDefault();
-    if (validateForm()) {
-      try {
-        const user = await signIn(form);
-        setUser(user);
-        navigate("/homepage");
-      } catch (error) {
-        console.error(error);
-        setForm({
-          isError: true,
-          errorMsg: "Invalid Credentials",
-          username: "",
-          password: "",
-        });
-      }
+    try {
+      const user = await signIn(form);
+      setUser(user);
+      navigate("/homepage");
+    } catch (error) {
+      console.error(error);
+      setForm({
+        isError: true,
+        errorMsg: "Invalid Credentials",
+        username: "",
+        password: "",
+      });
     }
   };
 
   const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
     if (form.isError) {
-      return <p className="text-red-500">{form.errorMsg}</p>;
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return <button type="submit">Sign In</button>;
     }
-    return null;
   };
 
   const { username, password } = form;
 
   return (
     <>
-      <div className="w-64 h-76 bg-white rounded-lg shadow-md flex flex-col">
-        <form onSubmit={onLogin} className="space-y-4">
-          <label htmlFor="username" className="block">
-            Username
-          </label>
-          <input
-            required
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            placeholder="Username"
-            onChange={handleUser}
-            className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <label htmlFor="password" className="block">
-            Password
-          </label>
-          <input
-            required
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            placeholder="Password"
-            onChange={handleUser}
-            className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <div className="flex flex-col space-y-2 bg-blue">
-            {renderError()}
-            <button type="submit">Log In</button>
-          </div>
-          <p className="mt-2">
-            Don't have an account?{" "}
-            <a href="/signup" className="text-blue-500 hover:underline">
-              Sign up
-            </a>
-          </p>
-        </form>
-      </div>
-    </>
+    <div className="form-signin">
+      <h3>Sign In</h3>
+      <form onSubmit={onSignIn}>
+        <label>Username</label>
+        <input
+          required
+          type="text"
+          name="username"
+          value={username}
+          placeholder="Enter Username"
+          onChange={handleUser}
+        />
+        <label>Password</label>
+        <input
+          required
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={handleUser}
+        />
+        {renderError()}
+      </form>
+      <p>Need an account? <a href="/signup">Sign up</a></p>
+ </div>
+ </>
   );
 }
-
 export default Login;
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,93 +1,112 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { signUp } from "../../services/users.js";
-import './SignUp.css'
+import './SignUp.css';
 
 function SignUp(props) {
   const { setUser } = props;
   const navigate = useNavigate();
-  const [form, setForm] = useState({
-    username: "",
-    password: "",
-  });
+  const { register, handleSubmit, formState: { errors }, getValues } = useForm();
 
-  const handleChange = (event) =>
-    setForm({
-      ...form,
-      [event.target.name]: event.target.value,
-    });
-
-  const onSignUp = async (event) => {
-    event.preventDefault();
+  const onSignUp = async (data) => {
     try {
-      const user = await signUp(form);
+      const user = await signUp(data);
       setUser(user);
-      console.log(user)
+      console.log(user);
       navigate("/create");
     } catch (error) {
       console.error(error);
-      setForm({
-        username: "",
-        password: "",
-        passwordConfirmation: "",
-        isError: true,
-        errorMsg: "Sign Up Details Invalid",
-      })
     }
   };
 
-  const renderError = () => {
-    const toggleForm = form.isError ? "danger" : "";
-    if (form.isError) {
-      return (
-        <button type="submit" className={toggleForm}>
-          {form.errorMsg}
-        </button>
-      );
-    } else {
-      return <button type="submit">Sign Up</button>;
+  const renderError = (fieldName) => {
+    if (errors[fieldName]) {
+      return <span>{errors[fieldName].message}</span>;
     }
+    return null;
   };
-
-  const { username, password, passwordConfirmation } = form;
-
-  console.log(form)
 
   return (
     <>
       <div className="form-signup">
         <h3>Sign Up</h3>
-        <form onSubmit={onSignUp}>
+        <form onSubmit={handleSubmit(onSignUp)}>
+          <label>Email</label>
+          <input 
+            required
+            type="email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address",
+              },
+            })}
+            placeholder="Enter email"
+          />
+          {renderError("email")}
+
           <label>Username</label>
           <input
             required
             type="text"
-            name="username"
-            value={username}
+            {...register("username", {
+              required: "Username is required",
+              pattern: {
+                value: /^[A-Za-z0-9]+$/,
+                message: "Username should only contain alphanumeric characters",
+              },
+              minLength: {
+                value: 5,
+                message: "Username should have a minimum length of 5",
+              },
+              maxLength: {
+                value: 24,
+                message: "Username should have a maximum length of 24",
+              },
+            })}
             placeholder="Enter username"
-            onChange={handleChange}
           />
+          {renderError("username")}
+
           <label>Password</label>
           <input
             required
-            name="password"
-            value={password}
             type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 5,
+                message: "Password should have a minimum length of 5",
+              },
+              maxLength: {
+                value: 24,
+                message: "Password should have a maximum length of 24",
+              },
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]+$/,
+                message: "Password should contain at least one uppercase letter, number, and symbol",
+              },
+            })}
             placeholder="Password"
-            onChange={handleChange}
           />
+          {renderError("password")}
+
           <label>Password Confirmation</label>
           <input
             required
-            name="passwordConfirmation"
-            value={passwordConfirmation}
             type="password"
+            {...register("passwordConfirmation", {
+              required: "Password confirmation is required",
+              validate: (value) => value === getValues("password") || "Passwords do not match",
+            })}
             placeholder="Confirm Password"
-            onChange={handleChange}
           />
-          {renderError()}
+          {renderError("passwordConfirmation")}
+
+          <button type="submit">Sign Up</button>
         </form>
-        <p>Have an account <a href="/">Sign in</a></p>
+        <p>Have an account? <a href="/login">Login</a></p>
       </div>
     </>
   );
